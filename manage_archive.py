b80cba70f23277cd7641f7e708434f974c4f0e05
naaberhaci <yucemusmula@gmail.com>
2025-04-09 23:02:05 +0200

    Özel numaralandırma & daha iyi program düzeni

--- manage_archive.py
+++ manage_archive.py
@@ -5,15 +5,41 @@ def confirm_and_move(old, new, yes):
     if yes:
         confirmation = "y"
     else:
-        confirmation = input(f"rename\n\033[1;32;40m{old}\033[0m\nto\n\033[1;32;40m{new}\033[0m? (enter/y to confirm): ")
+        confirmation = input(
+            f"rename\n\033[1;32;40m{old}\033[0m\nto\n\033[1;32;40m{new}\033"
+            f"[0m?\n(enter/y to confirm, c for custom number, other key to skip): "
+        )
+
+    def move_file(old, new):
+        # Move the file using subprocess
+        subprocess.run(["mv", old, new])
+        # Clear the last 5 lines, ensure no artifacts from previous print
+        sys.stdout.write("\x1b[1A\x1b[2K" * 5)
+        return 1
 
     if confirmation.lower() in ["y", "\n", "", "yes"]:
-        subprocess.run(["mv", old, new])
-        # Clear the last 4 lines, ensure no artifacts from previous print
-        sys.stdout.write("\x1b[1A\x1b[2K" * 4)
-        return 1
+        return move_file(old, new)
+    elif confirmation.lower() in ["c", "custom"]:
+        new_order = input("Enter custom order number: ")
+        if new_order.isdigit():
+            new_order_str = "[" + str(int(new_order)).zfill(4) + "] "
+
+            # Remove order name from beginning of new filename string ([*]) with regex
+            order_idx = new.find("]") + 2
+            new = new_order_str + new[order_idx:]
+
+            # Check if the new name already exists
+            if os.path.exists(new):
+                print(f"File {new} already exists, skipping", old)
+                return 0
+            
+            # Else, repeat the move with the new name
+            return confirm_and_move(old, new, yes)
+        else:
+            print("Invalid order number, skipping", old)
+            return 0
     else:
-        print("skipping", old)
+        print("Skipping", old)
         return 0
 
 
@@ -68,16 +94,16 @@ def rename(path, undo=False, yes=False):
                 new_file = "[" + str(no).zfill(4) + "] " + file
                 
                 # Fix bad chars in filenames
-                if r"Ä°" in new_file:
-                    new_file = new_file.replace(r"Ä°", "İ")
-                if r"Ä±" in new_file:
-                    new_file = new_file.replace(r"Ä±", "ı")
-
+                # (Check alternative encodings for same character with elifs):
                 if r"ÅÂ" in new_file:
                     new_file = new_file.replace(r"ÅÂ", "Ş")
                 elif r"Å" in new_file:
                     new_file = new_file.replace(r"Å", "Ş")
                 
+                if r"Ä°" in new_file:
+                    new_file = new_file.replace(r"Ä°", "İ")
+                if r"Ä±" in new_file:
+                    new_file = new_file.replace(r"Ä±", "ı")
                 if r"ÅŸ" in new_file:
                     new_file = new_file.replace(r"ÅŸ", "ş")
                 if r"Ä" in new_file:
@@ -103,7 +129,9 @@ def rename(path, undo=False, yes=False):
     print("Done, renamed", count, "files")
 
 
-def detect_missing(path, replace_missing):
+def detect_missing(path, replace_missing=True):
+    print("Detecting missing files...")
+
     # cd to the directory
     os.chdir(path)
     last_no = 0
@@ -135,25 +163,24 @@ def detect_missing(path, replace_missing):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage a comic book directory from ÇizgiDiyarı")
-    parser.add_argument("path", help="Path to the directory", nargs="?", default=".")
+    parser.add_argument("path", help="Path to the directory")
     parser.add_argument("--undo-rename", help="Undo the renaming", action="store_true")
     parser.add_argument("--yes", help="Skip confirmation", action="store_true")
-    parser.add_argument("--detect-missing", help="List missing files", action="store_true")
-    parser.add_argument("--replace-missing", help="Detect and replace missing files", action="store_true")
+    parser.add_argument("--no-detect-missing", help="Skip listing missing items", action="store_true")
+    parser.add_argument("--no-replace-missing", help="Skip replacing missing itema with blank file", action="store_true")
 
     args = parser.parse_args()
 
     path = args.path
     undo = args.undo_rename
     yes = args.yes
-    detect_missing_flag = args.detect_missing
-    replace_missing_flag = args.replace_missing
+    detect_missing_flag = not args.no_detect_missing
+    replace_missing_flag = not args.no_replace_missing
 
     if not os.path.exists(path):
         print("Path does not exist:", path)
         sys.exit(1)
 
-    if detect_missing_flag or replace_missing_flag:
+    rename(path, undo, yes)
+    if detect_missing_flag:
         detect_missing(path, replace_missing_flag)
-    else:
-        rename(path, undo, yes)
